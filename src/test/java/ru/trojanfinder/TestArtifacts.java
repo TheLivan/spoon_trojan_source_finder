ee1813d77ed7d5f6b5426075e72a5e51bf063ad7
Mark <markjarkovkod@gmail.com>
2025-10-22 16:29:47 +0300

    add negative test

--- src/test/java/ru/trojanfinder/TestArtifacts.java
+++ src/test/java/ru/trojanfinder/TestArtifacts.java
@@ -9,8 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class TestArtifacts {
 
     @Test
+    void testNegative() {
+        List<String> analyzeMessages = new Main().analyzeSources("./src/test/resources/Negative.java");
+        assertTrue(analyzeMessages.isEmpty());
+    }
+
+    @Test
     void testArtifact1() {
         List<String> analyzeMessages = new Main().analyzeSources("./src/test/resources/CommentingOut.java");
         assertTrue(analyzeMessages.size() == 1);
+        // code
+    }
+
+    @Test
+    void testArtifact2() {
+        // code
+    }
+
+    @Test
+    void testArtifact3() {
+        // code
     }
 }
